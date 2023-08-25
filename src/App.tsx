023ff05fddffb303de38a82380e43b1005e411fd
tsuka8888 <tks.ohtsu.fw77@gmail.com>
2023-08-25 21:38:42 +0900

    ✨ TOP画面を実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,7 @@
+import Title from "./Title/Title";
+import "modern-css-reset/dist/reset.min.css";
+import "./stylesheets/styles.scss";
+
 export default function App() {
-  return <div>App</div>;
+  return <Title />;
 }
---
+++ src/Title/Title.tsx
@@ -0,0 +1,20 @@
+import TitleSrc from "../assets/title.jpg";
+
+function Title() {
+  return (
+    <section id="title">
+      <div className="title-left">
+        <p className="portfolio small">ポートフォリオ</p>
+        <div className="name">
+          <p className="large">山本 司</p>
+          <p className="small">フロントエンドエンジニア</p>
+        </div>
+      </div>
+      <div className="title-right">
+        <img src={TitleSrc} alt="" />
+      </div>
+    </section>
+  );
+}
+
+export default Title;
