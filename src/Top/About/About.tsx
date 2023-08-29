f7fb0c7cb4816c0a4fa292a6158c4f5138b76a17
yamamoto.tsukasa <yamamoto.tsukasa@ditgroup.jp>
2023-08-29 15:40:01 +0900

    ♻️ Aboutコンポーネントを更新

--- src/Top/About/About.tsx
+++ src/Top/About/About.tsx
@@ -18,22 +18,48 @@ function getAge(year: number, month: number, day: number) {
 function About() {
   return (
     <section id="about">
-      <div className="about-name">
-        <img src={nigaoeSrc} alt="" />
-        <div className="about-name-name">
-          <p className="large">山本 司</p>
-          <p className="medium">フロントエンドエンジニア</p>
+      <div className="about-wrapper">
+        <div className="about-title">
+          <h1 className="font-large">ABOUT</h1>
+          <p>私について</p>
         </div>
-      </div>
-      <div className="about-description">
-        <p>
-          1994年12月生まれ {getAge(1994, 12, 5)}歳
-          <br />
-          フリーランスのフロントエンジニアです。
-          <br />
-          <br />
-          約6年半の間、会社員としてWeb開発
-        </p>
+        <div className="about-description">
+          <p>
+            1994年12月生まれ {getAge(1994, 12, 5)}歳
+            <br />
+            フリーランスのフロントエンジニアです。
+            <br />
+            <br />
+            2017年から2023年まで会社員としてWeb開発に携わった後、フリーランスに転向。
+            <br />
+            現在はReactとTypeScriptを用いたモダンフロントエンド開発を主に行っています。
+          </p>
+        </div>
+        <div className="about-name">
+          <img src={nigaoeSrc} alt="" />
+          <div className="about-table">
+            <div className="about-table-item">
+              <dt>名前：</dt>
+              <dd>山本司</dd>
+            </div>
+            <div className="about-table-item">
+              <dt>職業：</dt>
+              <dd>フロントエンドエンジニア</dd>
+            </div>
+            <div className="about-table-item">
+              <dt>所在地：</dt>
+              <dd>滋賀県</dd>
+            </div>
+            <div className="about-table-item">
+              <dt>言語など：</dt>
+              <dd>HTML、CSS、JavaScript、TypeScript、React、AWS、Git</dd>
+            </div>
+            <div className="about-table-item">
+              <dt>資格：</dt>
+              <dd>基本情報技術者、情報セキュリティマネジメント、AWS認定クラウドプラクティショナー、AWS認定ソリューションアーキテクトアソシエイト</dd>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
