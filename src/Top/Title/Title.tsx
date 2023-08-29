9924d7df1b3515de952093e5ae130913e6c2c5f9
yamamoto.tsukasa <yamamoto.tsukasa@ditgroup.jp>
2023-08-29 10:40:29 +0900

    ♻️ タイトルコンポーネントを更新

--- src/Top/Title/Title.tsx
+++ src/Top/Title/Title.tsx
@@ -1,17 +1,16 @@
-import TitleSrc from "../../assets/title.jpg";
+import NigaoeSrc from "../../assets/nigaoe.png";
 
 function Title() {
   return (
     <section id="title">
-      <div className="title-left">
-        <p className="portfolio small">ポートフォリオ</p>
+      <div className="title-wrapper">
         <div className="name">
-          <p className="large">山本 司</p>
+          <p className="large">YAMAMOTO TSUKASA</p>
           <p className="small">フロントエンドエンジニア</p>
         </div>
-      </div>
-      <div className="title-right">
-        <img src={TitleSrc} alt="" className="title-img"/>
+        <div className="icon">
+          <img src={NigaoeSrc} alt="" />
+        </div>
       </div>
     </section>
   );
