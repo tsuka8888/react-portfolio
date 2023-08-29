3824e53e08324ad380765a194d1842338eb28ab8
yamamoto.tsukasa <yamamoto.tsukasa@ditgroup.jp>
2023-08-29 15:57:39 +0900

    ✨ ナビゲーションを追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import "modern-css-reset/dist/reset.min.css";
 import "./stylesheets/styles.scss";
 import Top from "./Top/Top";
+import Header from "./Header/Header";
 
 export default function App() {
-  return <Top />;
+  return (
+    <>
+      <Header />
+      <Top />
+    </>
+  );
 }
---
+++ src/Header/Header.tsx
@@ -0,0 +1,12 @@
+function Header() {
+  return (
+    <nav id="navigation">
+      <ul className="navigation-wrapper">
+        <li>Top</li>
+        <li>About</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Header;
